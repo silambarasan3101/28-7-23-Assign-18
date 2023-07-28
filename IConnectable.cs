d75f53a115fabea5353c5994420b8c0de61b4e57
Silambarasan <msilambarasan98@gmail.com>
2023-07-28 15:29:51 +0530

    first commit

---
+++ IConnectable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_7_23_Assign_18
+{
+    public interface IConnectable
+    {
+        bool Connect();
+
+    }
+}
